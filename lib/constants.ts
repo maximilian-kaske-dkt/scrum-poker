06b5445fca9a55a5182bc0916784bf1654d851a4
maximilian-kaske <maximilian.kaske@decathlon.com>
2024-11-11 16:21:09 +0100

    fix: base url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.VERCEL_URL;
+export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
